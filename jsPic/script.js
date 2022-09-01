ca152aa17c3ee5b96a49fb428218fe05d1fe481f
mike <mikecokina@gmail.com>
2022-09-01 12:59:53 +0200

    jsPic - get image orientation, resolution, get squared thumbnail + fix in fetchCompositionImage (valid resolution)

--- jsPic/script.js
+++ jsPic/script.js
@@ -1,5 +1,34 @@
 $(function () {
     /**
+     * Get image resolution.
+     *
+     * @param {String|Promise} img
+     * @return {Promise<Object>}
+     */
+    const imageResolution = (img) => {
+        return new Promise((resolve, reject) => {
+            const image = img instanceof Promise ? img : loadImage(img)
+            image.then((im) => {
+                resolve({width: im?.width, height: im?.height})
+            }).catch((err) => {reject(err)})
+        })
+    }
+
+    /**
+     * Get image orientation portrait/landscape based on width/height ratio.
+     *
+     * @param {String|Promise} img
+     * @return {Promise<Object>}
+     */
+    const imageOrientation = (img) => {
+        return new Promise((resolve, reject) => {
+            imageResolution(img).then((res) => {
+                resolve(res.width / res.height > 1 ? 'landscape' : 'portrait')
+            }).catch((err) => { reject(err) })
+        })
+    }
+
+    /**
      * Python pop-like method.
      */
     Object.prototype.pop = function () {
@@ -37,6 +66,7 @@ $(function () {
                 reject(err)
             })
             image.setAttribute('src', imageSrc)
+            image.setAttribute('crossorigin', 'anonymous')
         })
     }
 
@@ -59,6 +89,7 @@ $(function () {
      * @param {Array<Number>} size
      * @param {Object} ctx
      * @param {Object} config
+     * @param {String} fillStyle
      * @return {Promise<String>}
      */
     const fetchImage = (
@@ -66,6 +97,7 @@ $(function () {
         size,
         ctx,
         config,
+        fillStyle = 'rgba(0, 0, 0, 0)'
     ) => {
         return new Promise((resolve, reject) => {
             Promise.all(images.map((entry) => {
@@ -81,8 +113,9 @@ $(function () {
                     // Prepare transparent base.
                     ctx.canvas.width = config.baseWidth
                     ctx.canvas.height = config.baseHeight
-                    ctx.fillStyle = 'rgba(0, 0, 0, 0)'
-                    ctx.fillRect(0, 0, config.width, config.height)
+
+                    ctx.fillStyle = fillStyle
+                    ctx.fillRect(0, 0, config.baseWidth, config.baseHeight)
 
                     for (const [i, img] of enumerate(images)) {
                         const conf = config.images[i]
@@ -122,21 +155,24 @@ $(function () {
      * @param {Array<String | Image | Promise>} images
      * @param {Array<Number>} size
      * @param {Object} config
+     * @param {String} fillStyle
      * @return {Object}
      */
     const fetchCompositionImage = (
         images,
         size,
         config,
+        fillStyle = 'rgba(0, 0, 0, 0)'
     ) => {
         // Create new canvas.
         const canvas = document.createElement('canvas')
         const ctx = canvas.getContext('2d');
+
         // Re-compute image size to desired resolution.
         const config_ = reScale(size, {...config})
 
         // Fetch image.
-        return fetchImage(images, size, ctx, config_)
+        return fetchImage(images, size, ctx, config_, fillStyle)
     }
 
 // Example
@@ -164,7 +200,7 @@ $(function () {
                 ]
             }
             const images = ["backgrounds/1007_DMA_Product Mockups_v2_1500px2.jpg", "portrait.png"]
-            const promise = fetchCompositionImage(images, [600, 600], conf)
+            const promise = fetchCompositionImage(images, [750, 750], conf)
 
             promise.then((result) => {
                 const contentElement = document.getElementById('content')
@@ -254,5 +290,235 @@ $(function () {
             })
         }
     }
-    main("string")
+
+    /**
+     * Create squared thumbnail of any image.
+     *
+     * @param {String|Promise} src
+     * @param {Number} size
+     * @param {String} bgColor
+     * @return {Promise}
+     */
+    const getSquaredThumbnail = (src, size, bgColor = 'rgba(0, 0, 0, 0)') => {
+        return new Promise(((resolve, reject) => {
+            Promise.all([imageResolution(src), imageOrientation(src)]).then(
+                ([resolution, orientation]) => {
+                    const a = Math.max(resolution.width, resolution.height)
+                    const conf = {
+                        baseWidth: a,
+                        baseHeight: a,
+                        images: [ {xPos: 0, yPos: 0, width: resolution.width, height: resolution.height}]
+                    }
+
+                    if (orientation === 'portrait') { conf.images[0].xPos = (a / 2) - (resolution.width / 2) }
+                    if (orientation === 'landscape') { conf.images[0].yPos = (a / 2) - (resolution.height / 2) }
+                    resolve(fetchCompositionImage([src], [size, size], conf, bgColor))
+                }
+            ).catch((err) => { reject(err) })
+        }))
+    }
+
+    const src = './landscape.png'
+    const im = loadImage(src)
+    getSquaredThumbnail(im, 150, 'rgba(0, 0, 0)').then((r) => {
+        $('#img').attr('src', String(r))
+    })
+
+
+    // main("string")
+    // const config = [
+    //     {
+    //         image: 'backgrounds/1007_DMA_Product Mockups_Light Wood_1.jpg',
+    //         orientation: 'portrait',
+    //         baseWidth: 1500,
+    //         baseHeight: 1500,
+    //         images: [
+    //             {
+    //                 xPos: 0,
+    //                 yPos: 0,
+    //                 height: 1500,
+    //                 width: 1500
+    //             },
+    //             {
+    //                 xPos: 661,
+    //                 yPos: 177,
+    //                 height: 673,
+    //                 width: 479
+    //             },
+    //         ]
+    //     },
+    //     {
+    //         image: 'backgrounds/1007_DMA_Product Mockups_Light Wood_2.jpg',
+    //         orientation: 'portrait',
+    //         baseWidth: 1500,
+    //         baseHeight: 1500,
+    //         images: [
+    //             {
+    //                 xPos: 0,
+    //                 yPos: 0,
+    //                 height: 1500,
+    //                 width: 1500
+    //             },
+    //             {
+    //                 xPos: 387,
+    //                 yPos: 220,
+    //                 height: 1013,
+    //                 width: 720
+    //             },
+    //         ]
+    //     },
+    //     {
+    //         image: 'backgrounds/1007_DMA_Product Mockups_Light Wood_3.jpg',
+    //         orientation: 'portrait',
+    //         baseWidth: 1500,
+    //         baseHeight: 1500,
+    //         images: [
+    //             {
+    //                 xPos: 0,
+    //                 yPos: 0,
+    //                 height: 1500,
+    //                 width: 1500
+    //             },
+    //             {
+    //                 xPos: 738,
+    //                 yPos: 326,
+    //                 height: 447,
+    //                 width: 318
+    //             },
+    //         ]
+    //     },
+    //     {
+    //         image: 'backgrounds/1007_DMA_Product Mockups_Light Wood_4.jpg',
+    //         orientation: 'portrait',
+    //         baseWidth: 1500,
+    //         baseHeight: 1500,
+    //         images: [
+    //             {
+    //                 xPos: 0,
+    //                 yPos: 0,
+    //                 height: 1500,
+    //                 width: 1500
+    //             },
+    //             {
+    //                 xPos: 565,
+    //                 yPos: 263,
+    //                 height: 675,
+    //                 width: 480
+    //             },
+    //         ]
+    //     },
+    //     {
+    //         image: 'backgrounds/1007_DMA_Product Mockups_Light Wood_5.jpg',
+    //         orientation: 'landscape',
+    //         baseWidth: 1500,
+    //         baseHeight: 1500,
+    //         images: [
+    //             {
+    //                 xPos: 0,
+    //                 yPos: 0,
+    //                 height: 1500,
+    //                 width: 1500
+    //             },
+    //             {
+    //                 xPos: 661,
+    //                 yPos: 297,
+    //                 height: 479,
+    //                 width: 673
+    //             },
+    //         ]
+    //     },
+    //     {
+    //         image: 'backgrounds/1007_DMA_Product Mockups_Light Wood_6.jpg',
+    //         orientation: 'landscape',
+    //         baseWidth: 1500,
+    //         baseHeight: 1500,
+    //         images: [
+    //             {
+    //                 xPos: 0,
+    //                 yPos: 0,
+    //                 height: 1500,
+    //                 width: 1500
+    //             },
+    //             {
+    //                 xPos: 315,
+    //                 yPos: 224,
+    //                 height: 720,
+    //                 width: 1013
+    //             },
+    //         ]
+    //     },
+    //     {
+    //         image: 'backgrounds/1007_DMA_Product Mockups_Light Wood_7.jpg',
+    //         orientation: 'landscape',
+    //         baseWidth: 1500,
+    //         baseHeight: 1500,
+    //         images: [
+    //             {
+    //                 xPos: 0,
+    //                 yPos: 0,
+    //                 height: 1500,
+    //                 width: 1500
+    //             },
+    //             {
+    //                 xPos: 720,
+    //                 yPos: 423,
+    //                 height: 320,
+    //                 width: 450
+    //             },
+    //         ]
+    //     },
+    //     {
+    //         image: 'backgrounds/1007_DMA_Product Mockups_Light Wood_8.jpg',
+    //         orientation: 'landscape',
+    //         baseWidth: 1500,
+    //         baseHeight: 1500,
+    //         images: [
+    //             {
+    //                 xPos: 0,
+    //                 yPos: 0,
+    //                 height: 1500,
+    //                 width: 1500
+    //             },
+    //             {
+    //                 xPos: 403,
+    //                 yPos: 415,
+    //                 height: 480,
+    //                 width: 675
+    //             },
+    //         ]
+    //     },
+    // ]
+    //
+    //
+    // const total = new Promise((resolve, reject) => {
+    //     const callback = {}
+    //     for (let [i, c] of enumerate(config)) {
+    //         const img_0 = c.pop('image')
+    //         const img_1 = c.pop('orientation') + ".png"
+    //         const images = [img_0, img_1]
+    //
+    //         const promise = fetchCompositionImage(images, [750, 750], c)
+    //         promise.then((result) => {
+    //             const imageElement = document.createElement('img')
+    //             imageElement.setAttribute('src', result)
+    //             const div = document.createElement('div')
+    //             div.setAttribute('id', i)
+    //             div.appendChild(imageElement)
+    //             callback[i] = div
+    //         })
+    //     }
+    //
+    //     let interval = setInterval(function () {
+    //         if (Object.entries(callback).length === 8) {
+    //             clearInterval(interval)
+    //             resolve(callback)
+    //         }
+    //     }, 100)
+    // })
+    //
+    // total.then((r) => {
+    //     const contentElement = document.getElementById('content')
+    //     for (let i = 0; i < Object.entries(r).length; ++i)
+    //         contentElement.appendChild(r[i])
+    // })
 })
